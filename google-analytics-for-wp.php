c21284797c8364fd4b8c3b6813949e43bbbc3cda
Chris Ferdinandi <hello@gomakethings.com>
2014-02-28 10:39:57 -0500

    Updated plugin URI

--- google-analytics-for-wp.php
+++ google-analytics-for-wp.php
@@ -3,7 +3,7 @@
 /* ======================================================================
 
 	Plugin Name: Google Analytics for WP
-	Plugin URI: http://cferdinandi.github.io/google-analytics/
+	Plugin URI: https://github.com/cferdinandi/google-analytics/
 	Description: Adds your Google Analytics script to WordPress. Add your Google Analytics ID under <a href="options-general.php?page=googanalytics_theme_options">Settings &rarr; Google Analytics</a>.
 	Version: 1.2
 	Author: Chris Ferdinandi
